aae6981c486c2bf9cccd7f22ea2452c99fa18b51
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-04-25 17:31:46 -0500

    Generating random color numbers

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,18 @@ const spawnButton = document.querySelector("#spawn-button");
 
 
 let colorBlocks = ["a", "b", "c", "d"];
+
+function getRandomNumber() {
+   return Math.floor(Math.random() * 255)
+}
 function spawn() {
+
+    const commonColor = getRandomNumber()
+
+    colorBlocks.forEach((_colorBlock, index) => {
+        colorBlocks[index] = "rgb(" + commonColor + "," + getRandomNumber() + "," + getRandomNumber() + ")"
+    })
+
     colorBlocks.forEach(colorBlock => {
        const divElement = document.createElement("div")
        divElement.textContent = colorBlock
